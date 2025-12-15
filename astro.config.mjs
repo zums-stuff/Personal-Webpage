3519a4206d4393982cf9ebdf0a738fdc02bba462
zums-stuff <jafet.hernandez.ed@gmail.com>
2025-12-15 00:05:20 -0600

    Fix: Agregar .nojekyll para permitir carpetas _astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://zums-stuff.github.io',
-  base: '/personal-webpage',
+  base: '/Personal-Webpage',
 });
